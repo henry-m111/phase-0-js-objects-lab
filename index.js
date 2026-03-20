d493f1924061547d2d074b258a5399601172c2a8
henry-m111 <henry.muchiri@student.moringaschool.com>
2026-03-20 12:18:44 +0300

    Final fix for property names and exports

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-//Write your code here
-
-
-
-//Needed for the tests to work. Don't modify
-module.exports = {
-  ...(typeof attendee !== 'undefined' && { attendee }),
-  ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
-  ...(typeof logTicketPrice !== 'undefined' && { logTicketPrice }),
-  ...(typeof updateTicketType !== 'undefined' && { updateTicketType }),
-  ...(typeof updateTicketPrice !== 'undefined' && { updateTicketPrice }),
-  ...(typeof removeEventProperty !== 'undefined' && { removeEventProperty }),
-  ...(typeof addCheckedInProperty !== 'undefined' && { addCheckedInProperty })
+// 1. Initialize Object (Double check property name casing!)
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
 };
-// This allows the test suite to see your code
+
+// 2. Functions (Ensure names match the lab exactly)
+function logAttendeeName(obj) {
+  console.log(obj.name);
+}
+
+function logTicketPrice(obj) {
+  console.log(obj.ticketPrice);
+}
+
+function updateTicketType(obj, newType) {
+  obj.ticketType = newType;
+}
+
+function updateTicketPrice(obj, newPrice) {
+  obj.ticketPrice = newPrice;
+}
+
+function removeEventProperty(obj) {
+  delete obj.event;
+}
+
+function addCheckedInProperty(obj) {
+  obj.checkedIn = true;
+}
+
+// 3. ESSENTIAL: Module Exports (The test suite MUST have these)
 module.exports = {
   attendee,
   logAttendeeName,
@@ -22,3 +42,4 @@ module.exports = {
   removeEventProperty,
   addCheckedInProperty
 };
+
